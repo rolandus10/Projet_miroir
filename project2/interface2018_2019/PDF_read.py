b6fe60269d38a77a23586de8827668f2898ca530
rolandus10 <rolandguy18@gmail.com>
2019-03-20 00:56:58 +0100

    ajout pdf

---
+++ project2/interface2018_2019/PDF_read.py
@@ -0,0 +1,9 @@
+
+
+fichier = open("TR__Plans_de_houdain/40-HOUDAIN_R+1.pdf", "r")
+
+content = fichier.read()
+
+fichier.close()
+
+print(content)
--- project2/interface2018_2019/essai_menu_interface.py
+++ project2/interface2018_2019/essai_menu_interface.py
@@ -1,5 +1,6 @@
 from tkinter import *
 
+from PIL import ImageTk, Image
 
 
 # creer une fenetre
@@ -10,13 +11,34 @@ window.title("Test Menu")
 window.geometry("1080x720")
 window.config(background='Black')
 
-# frame
-frame = Frame(window, bg='Black', bd=2, relief=SUNKEN)
+# ouvrir plan
+def premier_etage():
+    # creer une fenetre
+    window2 = Tk()
+    # personnalisation de la fenetre
+    window2.title("1er Etage")
+    window2.geometry("1920x1080")
+    window2.config(background='Black')
+    # image
+    #img = ImageTk.PhotoImage(Image.open("assets/iconfinder_navigation.png")) # PIL solution
+    img = PhotoImage(file="assets/iconfinder_navigation.png") # PIL solution
+    plan = Label(window2, image=img, text="Navigation", bg='#000000', fg='White', font=("Arial", 25))
+    plan.pack()
+    window2.mainloop()
 
+# frame
+frame = Frame(window, bg='Black', bd=2, relief="groove")
+
+# image
+
+#img = ImageTk.PhotoImage(Image.open("assets/iconfinder_navigation.png"))  # PIL solution
+#img.__size((10, 10), Image.ANTIALIAS)
 # ajout d'elements
 
-Navigation = Button(frame, text="Navigation",  bg='#000000', fg='White', font=("Arial", 25))
+Navigation = Button(frame, text="Navigation", bg='#000000', fg='White', font=("Arial", 25), command=premier_etage)
 Navigation.grid(row=0, column=0, padx=25, pady=25)
+
+#Navigation.config(image=img)
 
 PAE = Button(frame, text="PAE",  bg='#000000', fg='White', font=("Courrier", 25))
 PAE.grid(row=0, column=1, sticky=W, padx=25, pady=25)
@@ -25,7 +47,13 @@ PAE.grid(row=0, column=1, sticky=W, padx=25, pady=25)
 Activites = Button(frame, text="Activites",  bg='#000000', fg='White', font=("Courrier", 25))
 Activites.grid(row=0, column=2, sticky=W, padx=25, pady=25)
 
+Planning = Button(frame, text="Planning",  bg='#000000', fg='White', font=("Courrier", 25))
+Planning.grid(row=0, column=3, sticky=W, padx=25, pady=25)
+
 frame.pack(side=BOTTOM)
+
+
+
 
 # lancement de la fentre
 window.mainloop()
