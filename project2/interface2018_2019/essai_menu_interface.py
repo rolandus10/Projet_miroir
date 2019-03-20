fd6186881a2b80ad6f47151f8163ed4e9d28010e
rolandus10 <rolandguy18@gmail.com>
2019-03-20 15:40:21 +0100

    ouvrir dans navigateur

--- project2/interface2018_2019/essai_menu_interface.py
+++ project2/interface2018_2019/essai_menu_interface.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+import webbrowser
 from PIL import ImageTk, Image
 
 
@@ -13,32 +13,29 @@ window.config(background='Black')
 
 # ouvrir plan
 def premier_etage():
-    # creer une fenetre
-    window2 = Tk()
-    # personnalisation de la fenetre
-    window2.title("1er Etage")
-    window2.geometry("1920x1080")
-    window2.config(background='Black')
-    # image
-    #img = ImageTk.PhotoImage(Image.open("assets/iconfinder_navigation.png")) # PIL solution
-    img = PhotoImage(file="assets/iconfinder_navigation.png") # PIL solution
-    plan = Label(window2, image=img, text="Navigation", bg='#000000', fg='White', font=("Arial", 25))
-    plan.pack()
-    window2.mainloop()
+    # frame
+    frame2 = Frame(window, bg='Black', bd=2, relief="groove")
+    img2 = PhotoImage(file="assets/navigation.gif")
+    PAE2 = Canvas(frame2, width=500, height=500, bg='white')
+    PAE2.create_image(0, 0, anchor=NW, image=img2)
+    PAE2.pack()
+    frame2.pack()
+    webbrowser.open(r"C:\Users\amy\Desktop\Projet_miroir\project2\interface2018_2019\TR__Plans_de_houdain\40-HOUDAIN_R+1.pdf")
+
 
 # frame
 frame = Frame(window, bg='Black', bd=2, relief="groove")
 
 # image
 
-#img = ImageTk.PhotoImage(Image.open("assets/iconfinder_navigation.png"))  # PIL solution
-#img.__size((10, 10), Image.ANTIALIAS)
+img = ImageTk.PhotoImage(Image.open("TR__Plans_de_houdain/Screen Shot 2019-03-19 at 4.49.02 PM.png"))  # PIL solution
+#img.resize((10, 10), Image.ANTIALIAS)
 # ajout d'elements
 
 Navigation = Button(frame, text="Navigation", bg='#000000', fg='White', font=("Arial", 25), command=premier_etage)
 Navigation.grid(row=0, column=0, padx=25, pady=25)
 
-#Navigation.config(image=img)
+# Navigation.config(image=img)
 
 PAE = Button(frame, text="PAE",  bg='#000000', fg='White', font=("Courrier", 25))
 PAE.grid(row=0, column=1, sticky=W, padx=25, pady=25)
